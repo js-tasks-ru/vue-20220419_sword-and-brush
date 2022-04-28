a22bd254bbd082b5a97256119cf8e9df99e7172f
Red-Warrior <sword_and_brush@mail.ru>
2022-04-28 20:12:35 +0300

    MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,24 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+const App = defineComponent({
+  name: 'app',
+
+  data() {
+    return {
+      selectedMeetup: 1,
+      meetupTitle: '',
+    };
+  },
+
+  watch: {
+    selectedMeetup: {
+      async handler(meetupId) {
+        this.meetupTitle = await fetchMeetupById(meetupId).then((res) => res.title);
+      },
+      immediate: true,
+    },
+  },
+});
+
+createApp(App).mount('#app');
