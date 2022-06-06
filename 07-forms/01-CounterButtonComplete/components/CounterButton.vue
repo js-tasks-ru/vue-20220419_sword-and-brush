8d8f593f885eb4691a3cef5dfd4b55cc45786e36
Red-Warrior <sword_and_brush@mail.ru>
2022-06-06 20:36:24 +0300

    CounterButtonComplete

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,37 @@
 <template>
-  <button></button>
+  <button @click="increaseCounter">{{ localCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  data() {
+    return {
+      localCount: this.count,
+    };
+  },
+
+  watch: {
+    count() {
+      this.localCount = this.count;
+    },
+  },
+
+  methods: {
+    increaseCounter() {
+      this.localCount++;
+      this.$emit('update:count', this.localCount);
+    },
+  },
 };
 </script>
