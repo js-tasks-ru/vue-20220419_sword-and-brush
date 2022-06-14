466be31b024abcccb93dd140e7b45ef8797455fd
Red-Warrior <sword_and_brush@mail.ru>
2022-06-14 18:13:53 +0300

    UiLink: В шаблоне используется tag переданный через props, убрана привязка атрибутов

--- 06-wrappers/01-UiLink/components/UiLink.vue
+++ 06-wrappers/01-UiLink/components/UiLink.vue
@@ -1,24 +1,17 @@
 <template>
-  <component :is="tagItem.tag" :[tagItem.attr]="$attrs[tagItem.attr]" class="link">
+  <component :is="tag" class="link">
     <slot />
   </component>
 </template>
 
 <script>
-import { RouterLink } from 'vue-router';
-
 export default {
   name: 'UiLink',
 
   props: {
     tag: {
       type: [String, Object],
-    },
-  },
-
-  computed: {
-    tagItem() {
-      return this.tag === 'a' ? { tag: 'a', attr: 'href' } : { tag: RouterLink, attr: 'to' };
+      default: 'RouterLink',
     },
   },
 };
