dc90eeb4825c064b18eaa8a71b493daf2d77d471
Red-Warrior <sword_and_brush@mail.ru>
2022-08-29 21:03:20 +0300

    debouncedRef

--- 08-reactivity/04-debouncedRef/utils/debouncedRef.js
+++ 08-reactivity/04-debouncedRef/utils/debouncedRef.js
@@ -8,7 +8,18 @@ import debounce from 'lodash/debounce';
  * @returns {Ref<T>} - Новый ref, обновляющийся с debounce при обновлении исходного ref-а
  */
 export function debouncedRef(source, wait) {
-  const debounced = ref(undefined); // ...
-  // ...
+  const debounced = ref(source.value);
+
+  const updateValue = (newValue) => (debounced.value = newValue);
+  const debounceFunc = debounce(updateValue, wait);
+
+  watch(
+    source,
+    (newValue) => {
+      debounceFunc(newValue);
+    },
+    { immediate: true },
+  );
+
   return debounced;
 }
